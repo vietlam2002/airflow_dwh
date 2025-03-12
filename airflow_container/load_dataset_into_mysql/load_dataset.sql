675bdcc534e90fdd8cb350b1dd164dbc421407fc
vietlam2002 <vietlam050607@gmail.com>
2025-03-12 17:36:08 +0700

    create load database

---
+++ airflow_container/load_dataset_into_mysql/load_dataset.sql
@@ -0,0 +1,35 @@
+LOAD DATA local INFILE '/tmp/dataset/product_category_name_translation.csv'
+INTO TABLE product_category_name_translation FIELDS TERMINATED BY ',' enclosed by '"' LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA local INFILE '/tmp/dataset/olist_sellers_dataset.csv'
+INTO TABLE sellers FIELDS TERMINATED BY ',' enclosed by '"' LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA local INFILE '/tmp/dataset/olist_customers_dataset.csv'
+INTO TABLE customers FIELDS TERMINATED BY ',' enclosed by '"' LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA local INFILE '/tmp/dataset/olist_products_dataset.csv'
+INTO TABLE products FIELDS TERMINATED BY ',' enclosed by '"' LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+	
+LOAD DATA local INFILE '/tmp/dataset/olist_orders_dataset.csv'
+INTO TABLE orders FIELDS TERMINATED BY ',' enclosed by '"' LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA local INFILE '/tmp/dataset/olist_order_items_dataset.csv'
+INTO TABLE order_items FIELDS TERMINATED BY ',' enclosed by '"' LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA local INFILE '/tmp/dataset/olist_order_payments_dataset.csv'
+INTO TABLE payments FIELDS TERMINATED BY ',' enclosed by '"' LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA local INFILE '/tmp/dataset/olist_order_reviews_dataset.csv'
+INTO TABLE order_reviews FIELDS TERMINATED BY ',' enclosed by '"' LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA local INFILE '/tmp/dataset/olist_geolocation_dataset.csv'
+INTO TABLE geolocation FIELDS TERMINATED BY ',' enclosed by '"' LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
--- airflow_container/plugins/mysql_operator.py
+++ airflow_container/plugins/mysql_operator.py
@@ -1,1 +1,13 @@
+from airflow.hooks.mysql_hook import MySqlHook
+from support_processing import TemplateOperatorDB
+import logging
+from contextlib import closing
+import os
 
+class MySqlOperators:
+    def __init__(self, conn_id="mysql"):
+        try:
+            self.mysqlhook = MySqlHook(mysql_conn_id=conn_id)
+            self.mysql_conn = self.mysqlhook.get_conn()
+        except:
+            logging.error(f"Can't connect to {conn_id} database")
